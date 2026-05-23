10c8cca550fc11e10225e49c4072e71672d1aee1
Teddi <teddikao@gmail.com>
2026-05-23 20:32:07 +0800

    Add state to setup menu to keep track of original board x and y size

--- app/src/features/variants/variantEditor/setupEditor/stores/setupMenu.ts
+++ app/src/features/variants/variantEditor/setupEditor/stores/setupMenu.ts
@@ -12,6 +12,14 @@ type SetupMenuStore = {
 	isPiecesExpanded: boolean;
 	expandPieces: () => void;
 	collapsePieces: () => void;
+
+	originalBoardXSize: number | null,
+	updateOriginalBoardXSize: (boardXSize: number) => void;
+	resetOriginalBoardXSize: () => void;
+
+	originalBoardYSize: number | null,
+	updateOriginalBoardYSize: (boardYSize: number) => void;
+	resetOriginalBoardYSize: () => void;
 }
 
 const useSetupMenuStore = create<SetupMenuStore>((set) => ({
@@ -26,6 +34,14 @@ const useSetupMenuStore = create<SetupMenuStore>((set) => ({
 	isPiecesExpanded: false,
 	expandPieces: () => set({ isPiecesExpanded: true }),
 	collapsePieces: () => set({ isPiecesExpanded: false }),
+
+	originalBoardXSize: null,
+	updateOriginalBoardXSize: (boardXSize: number) => set({ originalBoardXSize: boardXSize }),
+	resetOriginalBoardXSize: () => set({ originalBoardXSize: null }),
+
+	originalBoardYSize: null,
+	updateOriginalBoardYSize: (boardYSize: number) => set({ originalBoardYSize: boardYSize }),
+	resetOriginalBoardYSize: () => set({ originalBoardYSize: null }),
 }))
 
 export default useSetupMenuStore;
