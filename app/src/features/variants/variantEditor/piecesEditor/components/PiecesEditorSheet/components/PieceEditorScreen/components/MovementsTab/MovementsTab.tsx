fcd3040613acb957142606d0a12ae31160c3363d
Teddi <teddikao@gmail.com>
2026-05-25 07:28:40 +0800

    Add explicit null checks for other areas of piece editor

--- app/src/features/variants/variantEditor/piecesEditor/components/PiecesEditorSheet/components/PieceEditorScreen/components/MovementsTab/MovementsTab.tsx
+++ app/src/features/variants/variantEditor/piecesEditor/components/PiecesEditorSheet/components/PieceEditorScreen/components/MovementsTab/MovementsTab.tsx
@@ -28,6 +28,7 @@ import type {
 	RegularMove,
 } from "@/features/variants/common/types/pieceRules";
 import { ScrollArea } from "@/components/ui/scroll-area";
+import { isNullOrUndefined } from "@/shared/utils/typeChecks";
 
 export function MovementsTab() {
 	const {
@@ -166,7 +167,7 @@ export function MovementsTab() {
 
 	function handleMovementSelection(movementName: string) {
 		if (!pieceRulesetDraft) return;
-		if (!pieceName) return;
+		if (isNullOrUndefined(pieceName)) return;
 
 		const updatedPieceRulesetDraft = structuredClone(pieceRulesetDraft);
 
