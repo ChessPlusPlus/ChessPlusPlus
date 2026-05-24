de65bef65ec2771f32ff2951097b701ce5a07918
Teddi <teddikao@gmail.com>
2026-05-24 19:34:47 +0800

    Add markup for add player dialog

---
+++ app/src/features/variants/variantEditor/setupEditor/components/AddPlayerDialog.tsx
@@ -0,0 +1,33 @@
+import { Button } from "@/components/ui/button";
+import { Dialog, DialogTitle, DialogHeader, DialogContent, DialogDescription, DialogFooter } from "@/components/ui/dialog";
+import { Field, FieldError, FieldLabel } from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+
+function AddPlayerDialog() {
+	return (
+		<Dialog>
+			<DialogContent>
+				<DialogHeader>
+					<DialogTitle>Add player</DialogTitle>
+					<DialogDescription>
+						Add a new player to the setup.
+					</DialogDescription>
+				</DialogHeader>
+
+				<Field>
+					<FieldLabel htmlFor="playerNameInput">
+						Player name
+					</FieldLabel>
+					<Input id="playerNameInput" type="text" placeholder="Enter player name" />
+					<FieldError errors={[]} />
+				</Field>
+
+				<DialogFooter>
+					<Button className="w-full">Add player</Button>
+				</DialogFooter>
+			</DialogContent>
+		</Dialog>
+	);
+}
+
+export default AddPlayerDialog;
