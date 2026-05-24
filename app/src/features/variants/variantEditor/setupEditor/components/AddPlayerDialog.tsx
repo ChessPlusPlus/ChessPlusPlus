3319ef520cdf0bc612ef2d0e9902e707630fa517
Teddi <teddikao@gmail.com>
2026-05-24 19:39:17 +0800

    Add relevant props to input and field error components

--- app/src/features/variants/variantEditor/setupEditor/components/AddPlayerDialog.tsx
+++ app/src/features/variants/variantEditor/setupEditor/components/AddPlayerDialog.tsx
@@ -1,20 +1,42 @@
 import { Button } from "@/components/ui/button";
-import { Dialog, DialogTitle, DialogHeader, DialogContent, DialogDescription, DialogFooter } from "@/components/ui/dialog";
+import {
+	Dialog,
+	DialogTitle,
+	DialogHeader,
+	DialogContent,
+	DialogDescription,
+	DialogFooter,
+} from "@/components/ui/dialog";
 import { Field, FieldError, FieldLabel } from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
 import useAddPlayerDialogStore from "@/features/variants/variantEditor/setupEditor/stores/addPlayerDialog";
+import type { ChangeEvent } from "react";
 
 function AddPlayerDialog() {
-	const { isAddPlayerDialogOpen, openAddPlayerDialog, closeAddPlayerDialog } = useAddPlayerDialogStore();
+	const {
+		isAddPlayerDialogOpen,
+		openAddPlayerDialog,
+		closeAddPlayerDialog,
+		playerName,
+		updatePlayerName,
+		playerNameErrors,
+	} = useAddPlayerDialogStore();
+
+	function handlePlayerNameInputChange(e: ChangeEvent<HTMLInputElement>) {
+		updatePlayerName(e.target.value);
+	}
 
 	return (
-		<Dialog open={isAddPlayerDialogOpen} onOpenChange={(open) => {
-			if (open) {
-				openAddPlayerDialog();
-			} else {
-				closeAddPlayerDialog();
-			}
-		}}>
+		<Dialog
+			open={isAddPlayerDialogOpen}
+			onOpenChange={(open) => {
+				if (open) {
+					openAddPlayerDialog();
+				} else {
+					closeAddPlayerDialog();
+				}
+			}}
+		>
 			<DialogContent>
 				<DialogHeader>
 					<DialogTitle>Add player</DialogTitle>
@@ -27,8 +49,20 @@ function AddPlayerDialog() {
 					<FieldLabel htmlFor="playerNameInput">
 						Player name
 					</FieldLabel>
-					<Input id="playerNameInput" type="text" placeholder="Enter player name" />
-					<FieldError errors={[]} />
+					<Input
+						id="playerNameInput"
+						type="text"
+						placeholder="Enter player name"
+						value={playerName}
+						onChange={handlePlayerNameInputChange}
+						data-invalid={playerNameErrors.length > 0}
+						aria-invalid={playerNameErrors.length > 0}
+					/>
+					<FieldError
+						errors={playerNameErrors.map((errorMessage) => ({
+							message: errorMessage,
+						}))}
+					/>
 				</Field>
 
 				<DialogFooter>
