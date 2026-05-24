35616aecee2937c33a5baefbafe67c7bc5b37925
Teddi <teddikao@gmail.com>
2026-05-25 07:49:52 +0800

    Consistently use trimmed name

--- app/src/features/variants/variantEditor/piecesEditor/stores/piecesEditor.ts
+++ app/src/features/variants/variantEditor/piecesEditor/stores/piecesEditor.ts
@@ -334,13 +334,15 @@ const usePiecesEditorStore = create<PiecesEditorStore>((set, get) => ({
 			if (Object.keys(pieceEditorChanges).includes("pieceName")) {
 				if (isNullOrUndefined(pieceEditorChanges.pieceName)) return;
 
-				if (pieceEditorChanges.pieceName.trim() === "") {
+				const trimmedPieceName = pieceEditorChanges.pieceName.trim();
+
+				if (trimmedPieceName === "") {
 					get().updatePieceName(originalPieceName);
 					get().removePieceEditorChanges(["pieceName"]);
 					return;
 				};
 
-				if (Object.keys(pieceRulesetDraft).includes(pieceEditorChanges.pieceName.trim())) {
+				if (Object.keys(pieceRulesetDraft).includes(trimmedPieceName)) {
 					get().updatePieceName(originalPieceName);
 					get().removePieceEditorChanges(["pieceName"]);
 					return;
@@ -351,7 +353,7 @@ const usePiecesEditorStore = create<PiecesEditorStore>((set, get) => ({
 					updatedPieceRulesetDraft,
 					updatedSetupRulesDraft,
 					newPieceInfo,
-					pieceEditorChanges.pieceName,
+					trimmedPieceName,
 				);
 			}
 
