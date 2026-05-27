0628bf389e87df92abc7d4534b98cc952ee05e48
Teddi <teddikao@gmail.com>
2026-05-27 17:53:54 +0800

    Create basic file export logic

--- app/src/features/variants/variantEditor/json/components/JSONOptionsMenu.tsx
+++ app/src/features/variants/variantEditor/json/components/JSONOptionsMenu.tsx
@@ -2,16 +2,42 @@ import {
 	DropdownMenuContent,
 	DropdownMenuItem,
 } from "@/components/ui/dropdown-menu";
+import useVariantsStore from "@/features/variants/common/stores/variantsStore";
+import useVariantDraftStore from "@/features/variants/variantEditor/common/stores/variantDraft";
 import { IconFileExport, IconFileImport } from "@tabler/icons-react";
 
 function JSONOptionsMenu() {
+	const { currentVariantId } = useVariantDraftStore();
+	const { variants } = useVariantsStore();
+
+	function handleExportJSON() {
+		if (!currentVariantId) return;
+
+		const variant = variants[currentVariantId];
+		if (!variant) return;
+
+		const json = JSON.stringify(variant, null, 2);
+		const fileName = `${variant.variantName}.json`;
+
+		const blob = new Blob([json], { type: "application/json" });
+		const objectUrl = URL.createObjectURL(blob);
+
+		const a = document.createElement("a");
+		a.href = objectUrl;
+		a.download = fileName;
+
+		a.click();
+		a.remove();
+		URL.revokeObjectURL(objectUrl);
+	}
+
 	return (
-		<DropdownMenuContent  side="left" sideOffset={8} className="w-max p-2">
+		<DropdownMenuContent side="left" sideOffset={8} className="w-max p-2">
 			<DropdownMenuItem>
 				<IconFileImport className="size-4" />
 				Import JSON
 			</DropdownMenuItem>
-			<DropdownMenuItem>
+			<DropdownMenuItem onClick={handleExportJSON}>
 				<IconFileExport className="size-4" />
 				Export JSON
 			</DropdownMenuItem>
