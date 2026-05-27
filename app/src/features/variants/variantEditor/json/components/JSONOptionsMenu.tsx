c739af7fe1f582414a3c704b0c206e7ddba35759
Teddi <teddikao@gmail.com>
2026-05-27 21:46:22 +0800

    Add logic to open JSON import dialog

--- app/src/features/variants/variantEditor/json/components/JSONOptionsMenu.tsx
+++ app/src/features/variants/variantEditor/json/components/JSONOptionsMenu.tsx
@@ -3,11 +3,14 @@ import {
 	DropdownMenuItem,
 } from "@/components/ui/dropdown-menu";
 import ExportJSONDialog from "@/features/variants/variantEditor/json/components/ExportJSONDialog";
+import ImportJSONDialog from "@/features/variants/variantEditor/json/components/ImportJSONDialog";
 import useExportJSONDialogStore from "@/features/variants/variantEditor/json/stores/exportJSONDialog";
+import useImportJSONDialogStore from "@/features/variants/variantEditor/json/stores/importJSONDialog";
 import { IconFileExport, IconFileImport } from "@tabler/icons-react";
 
 function JSONOptionsMenu() {
 	const { openExportJSONDialog } = useExportJSONDialogStore();
+	const { openImportJSONDialog } = useImportJSONDialogStore();
 
 	return (
 		<>
@@ -16,7 +19,7 @@ function JSONOptionsMenu() {
 				sideOffset={8}
 				className="w-max p-2"
 			>
-				<DropdownMenuItem>
+				<DropdownMenuItem onClick={openImportJSONDialog}>
 					<IconFileImport className="size-4" />
 					Import JSON
 				</DropdownMenuItem>
@@ -27,6 +30,7 @@ function JSONOptionsMenu() {
 			</DropdownMenuContent>
 
 			<ExportJSONDialog />
+			<ImportJSONDialog />
 		</>
 	);
 }
