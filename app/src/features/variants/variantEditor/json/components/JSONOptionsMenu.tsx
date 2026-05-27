a9555dfb9eef9be759fef2a0f3851ba827392da6
Teddi <teddikao@gmail.com>
2026-05-27 17:07:22 +0800

    Remove open and onOpenChange handlers

--- app/src/features/variants/variantEditor/json/components/JSONOptionsMenu.tsx
+++ app/src/features/variants/variantEditor/json/components/JSONOptionsMenu.tsx
@@ -1,30 +1,22 @@
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem } from "@/components/ui/dropdown-menu";
-import useSidebarStore from "@/features/variants/variantEditor/common/stores/sidebar";
+import {
+	DropdownMenuContent,
+	DropdownMenuItem,
+} from "@/components/ui/dropdown-menu";
 import { IconLogin2, IconLogout2 } from "@tabler/icons-react";
 
 function JSONOptionsMenu() {
-	const { currentOpenMenu, updateCurrentOpenMenu, clearCurrentOpenMenu } = useSidebarStore();
-	
 	return (
-		<DropdownMenu open={currentOpenMenu === "jsonOptions"} onOpenChange={(open) => {
-			if (open) {
-				updateCurrentOpenMenu("jsonOptions");
-			} else {
-				clearCurrentOpenMenu();
-			}
-		}}>
-			<DropdownMenuContent>
-				<DropdownMenuItem>
-					<IconLogin2 />
-					Import JSON
-				</DropdownMenuItem>
-				<DropdownMenuItem>
-					<IconLogout2 />
-					Export JSON
-				</DropdownMenuItem>
-			</DropdownMenuContent>
-		</DropdownMenu>
-	)
+		<DropdownMenuContent>
+			<DropdownMenuItem>
+				<IconLogin2 />
+				Import JSON
+			</DropdownMenuItem>
+			<DropdownMenuItem>
+				<IconLogout2 />
+				Export JSON
+			</DropdownMenuItem>
+		</DropdownMenuContent>
+	);
 }
 
 export default JSONOptionsMenu;
