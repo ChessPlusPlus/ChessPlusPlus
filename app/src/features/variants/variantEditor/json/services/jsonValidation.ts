6122ec7e9916277c4197d2081b84dc09e1501b50
Teddi <teddikao@gmail.com>
2026-05-27 22:35:29 +0800

    Add response shape to JSON validation function

--- app/src/features/variants/variantEditor/json/services/jsonValidation.ts
+++ app/src/features/variants/variantEditor/json/services/jsonValidation.ts
@@ -1,17 +1,27 @@
 import api from "@/app/api";
 import { AxiosError } from "axios";
 
-async function validateJSON(json: Record<string, unknown>) {
+type JSONValidationResponse = {
+	validationStatus: boolean;
+	validationMessage: string;
+};
+
+async function validateJSON(json: Record<string, unknown>): Promise<JSONValidationResponse | null> {
 	try {
 		const response = await api.post("/json-validator-test", {
 			jsonToValidate: json,
 		});
 
-		return response.data;
+		return {
+			validationStatus: response.data.validationStatus[0],
+			validationMessage: response.data.validationStatus[1],
+		};
 	} catch (error) {
 		if (error instanceof AxiosError) {
 			console.error(error.response);
 		}
+
+		return null;
 	}
 }
 
