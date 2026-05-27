0d4080f8e5f337f0422137c610ce17d52fdf0e37
Teddi <teddikao@gmail.com>
2026-05-27 18:06:29 +0800

    Elimate image ID fields from pieces

--- app/src/features/variants/variantEditor/json/utils/serialiser.ts
+++ app/src/features/variants/variantEditor/json/utils/serialiser.ts
@@ -4,7 +4,7 @@ type ExportedJSON = {
 	setup: Record<string, unknown>;
 	moves: Record<string, unknown>;
 	pieces: Record<string, unknown>;
-}
+};
 
 function serialiseJSONForExport(rawJSON: VariantInfo) {
 	const newJSON: ExportedJSON = {
@@ -16,6 +16,21 @@ function serialiseJSONForExport(rawJSON: VariantInfo) {
 	newJSON.setup = rawJSON.variantRules.setupRules;
 	newJSON.moves = rawJSON.variantRules.movementRules;
 	newJSON.pieces = rawJSON.variantRules.pieceRuleset;
+
+	newJSON.pieces = Object.fromEntries(
+		Object.entries(newJSON.pieces).map(([pieceName, pieceRuleset]) => {
+			console.log(pieceName, pieceRuleset);
+
+			return [
+				pieceName,
+				Object.fromEntries(
+					Object.entries(pieceRuleset as Record<string, unknown>).filter(
+						([key]) => key !== "imageId",
+					),
+				),
+			];
+		}),
+	);
 
 	return newJSON;
 }
