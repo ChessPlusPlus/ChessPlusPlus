260948c1a6704c28547007e6cc3f36fdb80dd521
Teddi <teddikao@gmail.com>
2026-05-28 07:42:23 +0800

    Create function to convert dict to camel case

--- app/src/features/variants/common/utils/caseConverter.ts
+++ app/src/features/variants/common/utils/caseConverter.ts
@@ -5,23 +5,28 @@ function convertStringToSnakeCase(str: string) {
 		.trim();
 }
 
+function convertStringToCamelCase(str: string) {
+	return str.replace(/_([a-z])/g, (_, letter) => letter.toUpperCase()).trim();
+}
+
 function convertDictToSnakeCase(
 	dict: Record<string, unknown>,
 	excludeLevels: number[] = [],
+	convertTo: "snake" | "camel" = "snake",
 ) {
 	function _convert(
 		data: Record<string, unknown>,
 		currentLevel: number,
 	): Record<string, unknown> | Record<string, unknown>[] {
-		if (
-			data instanceof Object &&
-			data !== null &&
-			!Array.isArray(data)
-		) {
+		if (data instanceof Object && data !== null && !Array.isArray(data)) {
 			return Object.fromEntries(
 				Object.entries(data).map(([key, value]) => {
 					return [
-						excludeLevels.includes(currentLevel) ? key : convertStringToSnakeCase(key),
+						excludeLevels.includes(currentLevel)
+							? key
+							: convertTo === "snake"
+								? convertStringToSnakeCase(key)
+								: convertStringToCamelCase(key),
 						_convert(
 							value as Record<string, unknown>,
 							currentLevel + 1,
@@ -41,21 +46,4 @@ function convertDictToSnakeCase(
 	return _convert(dict, 0);
 }
 
-console.log(
-	convertDictToSnakeCase(
-		{
-			keyTest: "valueTest",
-			camelCaseTest: {
-				camelCaseKeyTest: "camelCaseValueTest",
-			},
-			camelCaseArrayTest: [
-				{
-					camelCaseKeyTest: "camelCaseValueTest",
-				},
-			],
-		},
-		[0],
-	),
-);
-
-export { convertDictToSnakeCase };
+export { convertDictToSnakeCase, convertStringToCamelCase };
