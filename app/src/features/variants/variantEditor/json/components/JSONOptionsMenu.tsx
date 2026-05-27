cb79020a0943973ee86109af30e801bcb10bc238
Teddi <teddikao@gmail.com>
2026-05-27 22:23:47 +0800

    Move jSON import to home page

--- app/src/features/variants/variantEditor/json/components/JSONOptionsMenu.tsx
+++ app/src/features/variants/variantEditor/json/components/JSONOptionsMenu.tsx
@@ -3,14 +3,11 @@ import {
 	DropdownMenuItem,
 } from "@/components/ui/dropdown-menu";
 import ExportJSONDialog from "@/features/variants/variantEditor/json/components/ExportJSONDialog";
-import ImportJSONDialog from "@/features/variants/variantEditor/json/components/ImportJSONDialog";
 import useExportJSONDialogStore from "@/features/variants/variantEditor/json/stores/exportJSONDialog";
-import useImportJSONDialogStore from "@/features/variants/variantEditor/json/stores/importJSONDialog";
-import { IconFileExport, IconFileImport } from "@tabler/icons-react";
+import { IconFileExport } from "@tabler/icons-react";
 
 function JSONOptionsMenu() {
 	const { openExportJSONDialog } = useExportJSONDialogStore();
-	const { openImportJSONDialog } = useImportJSONDialogStore();
 
 	return (
 		<>
@@ -19,10 +16,6 @@ function JSONOptionsMenu() {
 				sideOffset={8}
 				className="w-max p-2"
 			>
-				<DropdownMenuItem onClick={openImportJSONDialog}>
-					<IconFileImport className="size-4" />
-					Import JSON
-				</DropdownMenuItem>
 				<DropdownMenuItem onClick={openExportJSONDialog}>
 					<IconFileExport className="size-4" />
 					Export JSON
@@ -30,7 +23,6 @@ function JSONOptionsMenu() {
 			</DropdownMenuContent>
 
 			<ExportJSONDialog />
-			<ImportJSONDialog />
 		</>
 	);
 }
