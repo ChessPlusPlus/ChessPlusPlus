6d5c1b29e94c1d6d8bd248377686f432c5170d70
Teddi <teddikao@gmail.com>
2026-05-28 09:59:24 +0800

    Center images in setup chessboard

--- app/src/features/variants/variantEditor/setupEditor/components/SetupChessboard/PieceImage.tsx
+++ app/src/features/variants/variantEditor/setupEditor/components/SetupChessboard/PieceImage.tsx
@@ -12,12 +12,19 @@ function PieceImage({ piece, imageUrl, file, rank }: PieceImageProps) {
 		id: `${file}_${rank}-${piece}`,
 		data: {
 			startLocation: [file, rank],
-		}
+		},
 	});
 
 	if (!imageUrl) return null;
 
-	return <img ref={ref} src={imageUrl} alt={piece} />;
+	return (
+		<img
+			className="size-9/10 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+			ref={ref}
+			src={imageUrl}
+			alt={piece}
+		/>
+	);
 }
 
 export default PieceImage;
