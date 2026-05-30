a5f7771244f25836f633a12e8f00ae2877dbea25
Teddi <teddikao@gmail.com>
2026-05-30 13:40:49 +0800

    Add function to reset piece editor sheet state

--- app/src/features/variants/variantEditor/piecesEditor/stores/piecesEditorSheet.ts
+++ app/src/features/variants/variantEditor/piecesEditor/stores/piecesEditorSheet.ts
@@ -3,11 +3,13 @@ import { create } from "zustand";
 type PiecesEditorSheetStore = {
     currentMode: "pieceSelection" | "pieceEditing";
     updateCurrentMode: (newMode: "pieceSelection" | "pieceEditing") => void;
+    resetPiecesEditorSheetState: () => void;
 };
 
 const usePiecesEditorSheetStore = create<PiecesEditorSheetStore>((set) => ({
     currentMode: "pieceSelection",
     updateCurrentMode: (newMode) => set({ currentMode: newMode }),
+    resetPiecesEditorSheetState: () => set({ currentMode: "pieceSelection" }),
 }));
 
 export default usePiecesEditorSheetStore;
