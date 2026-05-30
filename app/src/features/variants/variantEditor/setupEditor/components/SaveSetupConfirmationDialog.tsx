147ef015e3cbc4969836ab45e0cf298c28a6fe43
Teddi <teddikao@gmail.com>
2026-05-30 14:08:23 +0800

    Add open and onOpenChange handlers

--- app/src/features/variants/variantEditor/setupEditor/components/SaveSetupConfirmationDialog.tsx
+++ app/src/features/variants/variantEditor/setupEditor/components/SaveSetupConfirmationDialog.tsx
@@ -1,8 +1,33 @@
-import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle } from "@/components/ui/alert-dialog";
+import {
+	AlertDialog,
+	AlertDialogAction,
+	AlertDialogCancel,
+	AlertDialogContent,
+	AlertDialogDescription,
+	AlertDialogFooter,
+	AlertDialogHeader,
+	AlertDialogTitle,
+} from "@/components/ui/alert-dialog";
+import useSetupSaveConfirmationDialogStore from "@/features/variants/variantEditor/setupEditor/stores/setupSaveConfirmationDialog";
 
 function SaveSetupConfirmationDialog() {
+	const {
+		isSetupSaveConfirmationDialogOpen,
+		openSetupSaveConfirmationDialog,
+		closeSetupSaveConfirmationDialog,
+	} = useSetupSaveConfirmationDialogStore();
+
 	return (
-		<AlertDialog>
+		<AlertDialog
+			open={isSetupSaveConfirmationDialogOpen}
+			onOpenChange={(open) => {
+				if (open) {
+					openSetupSaveConfirmationDialog();
+				} else {
+					closeSetupSaveConfirmationDialog();
+				}
+			}}
+		>
 			<AlertDialogContent>
 				<AlertDialogHeader>
 					<AlertDialogTitle>Leave setup page?</AlertDialogTitle>
@@ -13,9 +38,15 @@ function SaveSetupConfirmationDialog() {
 				</AlertDialogHeader>
 
 				<AlertDialogFooter>
-					<AlertDialogAction variant="default" className="px-4">Save and leave</AlertDialogAction>
-					<AlertDialogAction variant="destructive" className="px-4">Leave without saving</AlertDialogAction>
-					<AlertDialogCancel className="px-4">Cancel</AlertDialogCancel>
+					<AlertDialogAction variant="default" className="px-4">
+						Save and leave
+					</AlertDialogAction>
+					<AlertDialogAction variant="destructive" className="px-4">
+						Leave without saving
+					</AlertDialogAction>
+					<AlertDialogCancel className="px-4">
+						Cancel
+					</AlertDialogCancel>
 				</AlertDialogFooter>
 			</AlertDialogContent>
 		</AlertDialog>
