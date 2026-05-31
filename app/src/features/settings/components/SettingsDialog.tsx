a23e009c2b5eaf59a8508f272d36d4cf745aab53
Teddi <teddikao@gmail.com>
2026-05-31 11:26:38 +0800

    Add analytics consent field

--- app/src/features/settings/components/SettingsDialog.tsx
+++ app/src/features/settings/components/SettingsDialog.tsx
@@ -1,36 +1,64 @@
-import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog";
+import {
+	Dialog,
+	DialogContent,
+	DialogHeader,
+	DialogTitle,
+} from "@/components/ui/dialog";
+import {
+	Field,
+	FieldContent,
+	FieldDescription,
+	FieldLabel,
+} from "@/components/ui/field";
+import { Switch } from "@/components/ui/switch";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import useSettingsDialogStore from "@/features/settings/stores/settingsDialog";
 
 function SettingsDialog() {
-	const { isSettingsDialogOpen, openSettingsDialog, closeSettingsDialog } = useSettingsDialogStore();
+	const { isSettingsDialogOpen, openSettingsDialog, closeSettingsDialog } =
+		useSettingsDialogStore();
 
 	return (
-		<Dialog open={isSettingsDialogOpen} onOpenChange={(open) => {
-			if (open) {
-				openSettingsDialog();
-			} else {
-				closeSettingsDialog();
-			}
-		}}>
-			<DialogContent>
+		<Dialog
+			open={isSettingsDialogOpen}
+			onOpenChange={(open) => {
+				if (open) {
+					openSettingsDialog();
+				} else {
+					closeSettingsDialog();
+				}
+			}}
+		>
+			<DialogContent className="min-w-1/2">
 				<DialogHeader>
 					<DialogTitle>Settings</DialogTitle>
 				</DialogHeader>
 
-				<Tabs orientation="vertical">
+				<Tabs orientation="vertical" className="flex flex-row gap-4">
 					<TabsList className="h-full">
 						<TabsTrigger value="analytics">Analytics</TabsTrigger>
-						<TabsTrigger value="danger-zone">Danger zone</TabsTrigger>
+						<TabsTrigger value="danger-zone">
+							Danger zone
+						</TabsTrigger>
 					</TabsList>
 
-					<TabsContent value="analytics">
-						
+					<TabsContent value="analytics" className="flex flex-row gap-4">
+						<Field orientation="horizontal">
+							<FieldContent>
+								<FieldLabel htmlFor="allow-analytics">Allow analytics</FieldLabel>
+								<FieldDescription>
+									Allow us to collect anonymous usage
+									data to help us improve Chess++.
+								</FieldDescription>
+							</FieldContent>
+						</Field>
+
+						<Switch id="allow-analytics" />
 					</TabsContent>
 				</Tabs>
 			</DialogContent>
 		</Dialog>
-	)
+	);
 }
 
 export default SettingsDialog;
