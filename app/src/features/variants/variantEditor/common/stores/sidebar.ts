9f48657d7997475a1fcac9fb747df8630a8fe6b9
Teddi <teddikao@gmail.com>
2026-05-31 11:36:28 +0800

    Add functionality to reset sidebar state

--- app/src/features/variants/variantEditor/common/stores/sidebar.ts
+++ app/src/features/variants/variantEditor/common/stores/sidebar.ts
@@ -4,12 +4,18 @@ type SidebarStore = {
     currentOpenMenu: "boardSettings" | "pieces" | "movements" | "jsonOptions" | null;
     updateCurrentOpenMenu: (openMenu: "boardSettings" | "pieces" | "movements" | "jsonOptions") => void;
     clearCurrentOpenMenu: () => void;
+
+    resetSidebarState: () => void;
 }
 
-const useSidebarStore = create<SidebarStore>((set) => ({
+const useSidebarStore = create<SidebarStore>((set, _get, store) => ({
     currentOpenMenu: null,
     updateCurrentOpenMenu: (openMenu) => set({ currentOpenMenu: openMenu }),
     clearCurrentOpenMenu: () => set({ currentOpenMenu: null }),
+
+    resetSidebarState: () => {
+        set(store.getInitialState())
+    },
 }))
 
 export default useSidebarStore;
