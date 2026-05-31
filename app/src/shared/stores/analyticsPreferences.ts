c79f41409d9304e2315c21a8c326a0f4e0a862b2
Teddi <teddikao@gmail.com>
2026-05-31 09:47:57 +0800

    Add null state for analytics enabled state to account for users who have not decided

--- app/src/shared/stores/analyticsPreferences.ts
+++ app/src/shared/stores/analyticsPreferences.ts
@@ -2,23 +2,25 @@ import { create } from "zustand";
 import { createJSONStorage, persist } from "zustand/middleware";
 
 type AnalyticsPreferencesStore = {
-	analyticsEnabled: boolean;
+	analyticsEnabled: boolean | null;
 	enableAnalytics: () => void;
 	disableAnalytics: () => void;
-}
+};
 
 const useAnalyticsPreferencesStore = create<AnalyticsPreferencesStore>()(
 	persist(
 		(set) => ({
-			analyticsEnabled: false,
+			analyticsEnabled: null,
 			enableAnalytics: () => set({ analyticsEnabled: true }),
 			disableAnalytics: () => set({ analyticsEnabled: false }),
 		}),
 		{
 			name: "analyticsPreferences",
 			storage: createJSONStorage(() => localStorage),
-			partialize: (state) => ({ analyticsEnabled: state.analyticsEnabled }),
-		}
+			partialize: (state) => ({
+				analyticsEnabled: state.analyticsEnabled,
+			}),
+		},
 	),
 );
 
