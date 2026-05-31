70e3824f63e8cdc9d2d1de5b6081b626d0cafc2e
Teddi <teddikao@gmail.com>
2026-05-31 21:00:32 +0800

    Add state to track draft name and email

--- app/src/features/feedbackCollection/stores/feedbackCollectionCredentialsForm.ts
+++ app/src/features/feedbackCollection/stores/feedbackCollectionCredentialsForm.ts
@@ -4,12 +4,24 @@ type FeedbackCollectionCredentialsFormStore = {
 	isFeedbackCollectionCredentialsFormOpen: boolean;
 	openFeedbackCollectionCredentialsForm: () => void;
 	closeFeedbackCollectionCredentialsForm: () => void;
+
+	nameDraft: string;
+	updateNameDraft: (nameDraft: string) => void;
+
+	emailDraft: string;
+	updateEmailDraft: (emailDraft: string) => void;
 }
 
 const useFeedbackCollectionCredentialsFormStore = create<FeedbackCollectionCredentialsFormStore>((set) => ({
 	isFeedbackCollectionCredentialsFormOpen: false,
 	openFeedbackCollectionCredentialsForm: () => set({ isFeedbackCollectionCredentialsFormOpen: true }),
 	closeFeedbackCollectionCredentialsForm: () => set({ isFeedbackCollectionCredentialsFormOpen: false }),
+
+	nameDraft: "",
+	updateNameDraft: (nameDraft) => set({ nameDraft }),
+
+	emailDraft: "",
+	updateEmailDraft: (emailDraft) => set({ emailDraft }),
 }))
 
 export default useFeedbackCollectionCredentialsFormStore;
