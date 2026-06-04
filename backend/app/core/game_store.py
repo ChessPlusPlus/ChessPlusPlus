ff43e61dc24a3a61b2fa180dfab8f1c3e5024691
Teddi <teddikao@gmail.com>
2026-06-04 14:25:58 +0800

    Improve cache manipualtion functions

--- backend/app/core/game_store.py
+++ backend/app/core/game_store.py
@@ -6,12 +6,19 @@ def create_game(game_id: str, rules: dict, game_state: dict):
 		"game_state": game_state,
 	}))
 
-def get_game_state(game_id: str):
+def get_game_info(game_id: str):
 	game_info = json.loads(redis_client.get(f"game:{game_id}"))
 	return game_info["game_state"]
 
+def get_game_state(game_id: str):
+	game_info = get_game_info(game_id)
+	return game_info["game_state"]
+
+def update_game_info(game_id: str, game_info: dict):
+	redis_client.set(f"game:{game_id}", json.dumps(game_info))
+
 def update_game_state(game_id: str, game_state: dict):
-	redis_client.set(f"game:{game_id}", json.dumps({
-		"rules": rules,
-		"game_state": game_state,
-	}))
+	game_info = get_game_info(game_id)
+	game_info["game_state"] = game_state
+	
+	update_game_info(game_id, game_info)
