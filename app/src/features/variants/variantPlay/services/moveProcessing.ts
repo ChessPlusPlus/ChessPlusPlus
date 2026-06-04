56e7157d62b5f7a1b9099119f407e0bd6d37e68a
Teddi <teddikao@gmail.com>
2026-06-04 20:11:03 +0800

    Use performance API

--- app/src/features/variants/variantPlay/services/moveProcessing.ts
+++ app/src/features/variants/variantPlay/services/moveProcessing.ts
@@ -16,13 +16,15 @@ async function generateLegalMoves(
 	currentPos: [number, number],
 ): Promise<GenerateLegalMovesResponse> {
 	try {
-		console.time("legal move service call");
+		const t0 = performance.now();
+
 		const response = await api.post("game/generate-legal-moves/", {
 			gameId,
 			currentPos,
 		});
-		console.timeLog("legal move service call");
-		console.timeEnd("legal move service call");
+		const t1 = performance.now();
+		
+		console.log(`Legal move service call took ${t1 - t0} milliseconds`);
 
 		console.log(response.data);
 
