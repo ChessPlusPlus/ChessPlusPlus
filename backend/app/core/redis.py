f0920083c95c09d89d1d81b19e305d144be2c2e5
Teddi <teddikao@gmail.com>
2026-06-04 18:43:31 +0800

    Add connection pooling

--- backend/app/core/redis.py
+++ backend/app/core/redis.py
@@ -1,4 +1,6 @@
-from redis import Redis
+from redis import Redis, ConnectionPool
 from app.config import settings
 
-redis_client = Redis.from_url(settings.redis_url)
+pool = ConnectionPool.from_url(settings.redis_url, decode_responses=True, max_connections=50)
+
+redis_client = Redis(connection_pool=pool)
