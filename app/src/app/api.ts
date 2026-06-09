3172c7caf8a3534cac6a6e0fb789431e963c5cda
Teddi <teddikao@gmail.com>
2026-06-09 14:15:56 +0800

    Remove logs

--- app/src/app/api.ts
+++ app/src/app/api.ts
@@ -4,15 +4,4 @@ const api = axios.create({
 	baseURL: import.meta.env.VITE_API_BASE_URL,
 });
 
-api.interceptors.request.use((config) => {
-	console.log("=== AXIOS REQUEST ===");
-	console.log("baseURL:", config.baseURL);
-	console.log("url:", config.url);
-	console.log("method:", config.method);
-	console.log("full:", (config.baseURL || "") + (config.url || ""));
-	console.log("=====================");
-
-	return config;
-});
-
 export default api;
