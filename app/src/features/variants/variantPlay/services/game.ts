8b8073b54b2d6f4e4ea2c77d55bf7069374d5864
Teddi <teddikao@gmail.com>
2026-06-09 13:22:30 +0800

    Log base URL

--- app/src/features/variants/variantPlay/services/game.ts
+++ app/src/features/variants/variantPlay/services/game.ts
@@ -16,6 +16,8 @@ async function createGame(
 	pieceRuleset: PieceRuleset,
 	movementRules: MovementRules,
 ): Promise<CreateGameResponse> {
+	console.log("BASE URL:", import.meta.env.VITE_API_BASE_URL)
+	
 	try {
 		const response = await api.post("game/create-game/", {
 			setupRules,
