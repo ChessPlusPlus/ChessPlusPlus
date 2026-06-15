b07720a3615c017f9a09482b3aede73897d8463a
Teddi <teddikao@gmail.com>
2026-06-15 19:55:49 +0800

    Update functions to use game class

--- backend/app/core/game_store.py
+++ backend/app/core/game_store.py
@@ -1,42 +1,33 @@
 import json
+import pickle
 import time
 
 from app.core.redis import redis_client
+from backend.app.engine.legal_move_generator.legal_move_generator import Game
 
-async def create_game(game_id: str, rules: dict, game_state: dict):
-	await redis_client.set(f"game:{game_id}", json.dumps({
-		"rules": rules,
-		"game_state": game_state,
-	}))
+async def create_game(game_id: str, game_class: Game):
+	await redis_client.set(f"game:{game_id}", pickle.dumps(game_class))
 
-async def get_game_info(game_id: str):
+
+async def get_game_class(game_id: str):
 	store_retrieval_start = time.perf_counter()
-	game_info = await redis_client.get(f"game:{game_id}")
+	game_class = await redis_client.get(f"game:{game_id}")
 	store_retrieval_end = time.perf_counter()
 	time_taken = store_retrieval_end - store_retrieval_start
 	print(f"store_retrieval took {time_taken:.6f} seconds")
 	print(f"store_retrieval took {time_taken * 1000:.6f} milliseconds")
 
-	if game_info is None:
+	if game_class is None:
 		return None
 
-	return json.loads(game_info)
+	return pickle.loads(game_class)
 
 async def get_game_state(game_id: str):
-	game_info = await get_game_info(game_id)
-	if game_info is None:
+	game_class = await get_game_class(game_id)
+	if game_class is None:
 		return None
 
-	return game_info["game_state"]
+	return game_class.get_game_state()
 
-async def update_game_info(game_id: str, game_info: dict):
-	await redis_client.set(f"game:{game_id}", json.dumps(game_info))
-
-async def update_game_state(game_id: str, game_state: dict):
-	game_info = await get_game_info(game_id)
-	if game_info is None:
-		return None
-
-	game_info["game_state"] = game_state
-	
-	await update_game_info(game_id, game_info)
+async def update_game_class(game_id: str, game_class: Game):
+	await redis_client.set(f"game:{game_id}", pickle.dumps(game_class))
