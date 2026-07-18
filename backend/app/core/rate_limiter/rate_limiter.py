dce0f57b623389a2ff68a0f0feb7135fbdcadb2e
Teddi <teddikao@gmail.com>
2026-07-18 14:27:23 +0800

    Modify python rate limiter to return more detailed information

--- backend/app/core/rate_limiter/rate_limiter.py
+++ backend/app/core/rate_limiter/rate_limiter.py
@@ -10,6 +10,14 @@ script_content = script_path.read_text()
 run_consume_token_script = redis_client.register_script(script_content)
 
 async def consume_token(ip_address: str, bucket_size: int = BUCKET_SIZE, refill_rate: int = REFILL_AMOUNT, refill_interval: int = REFILL_INTERVAL, token_cost: int = 1):
-    result = await run_consume_token_script(keys=[f"token_bucket:{ip_address}"], args=[bucket_size, refill_rate, refill_interval, token_cost])
+    current_time = time.time()
+    result = await run_consume_token_script(keys=[f"token_bucket:{ip_address}"], args=[bucket_size, refill_rate, refill_interval, token_cost, current_time])
+    reset_time = result[3]
 
-    return bool(result)
+    return {
+        "success": result[0],
+        "bucket_size": result[1],
+        "remaining_tokens": result[2],
+        "retry_after": max(1, reset_time - current_time),
+        "reset_time": reset_time
+    }
