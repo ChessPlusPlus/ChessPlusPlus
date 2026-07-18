1251e5d0f3f723850aca179d3aebe4a44e06ed3b
Teddi <teddikao@gmail.com>
2026-07-18 15:37:31 +0800

    Streamline lua script by eliminating extra call

--- backend/app/core/rate_limiter/token_bucket.lua
+++ backend/app/core/rate_limiter/token_bucket.lua
@@ -5,22 +5,17 @@ local bucket_size = tonumber(ARGV[2])
 local refill_rate = tonumber(ARGV[3])
 local refill_interval = tonumber(ARGV[4])
 local current_time = tonumber(ARGV[5])
+local token_count = bucket_size
+local last_refill_time = current_time
 
 local raw_bucket_state = redis.call("GET", bucket_key)
-if raw_bucket_state == false then
-    redis.call("SET", bucket_key, cjson.encode({
-        tokens = bucket_size,
-        bucket_size = bucket_size,
-        refill_rate = refill_rate,
-        refill_interval = refill_interval,
-        last_refill_time = current_time,
-    }))
+if raw_bucket_state then
+    local bucket_state = cjson.decode(raw_bucket_state)
+
+    token_count = bucket_state["tokens"]
+    last_refill_time = bucket_state["last_refill_time"]
 end
 
-local current_bucket_state = cjson.decode(redis.call("GET", bucket_key))
-local token_count = current_bucket_state["tokens"]
-
-local last_refill_time = current_bucket_state["last_refill_time"]
 local elapsed_time = current_time - last_refill_time
 
 if elapsed_time >= refill_interval then
