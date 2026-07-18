cb3dc465ce3f55f2dbe2b053cbee1e1ebfbf3069
Teddi <teddikao@gmail.com>
2026-07-18 14:10:33 +0800

    Rename variable for clarity

--- backend/app/core/rate_limiter/rate_limiter.py
+++ backend/app/core/rate_limiter/rate_limiter.py
@@ -1,7 +1,7 @@
 from app.core.redis import redis_client
 
 BUCKET_SIZE = 20
-REFILL_RATE = 4
+REFILL_AMOUNT = 4
 REFILL_INTERVAL = 1
 
 script_path = Path(__file__).with_name("rate_limiter.lua")
@@ -9,7 +9,7 @@ script_content = script_path.read_text()
 
 run_consume_token_script = redis_client.register_script(script_content)
 
-async def consume_token(ip_address: str, bucket_size: int = BUCKET_SIZE, refill_rate: int = REFILL_RATE, refill_interval: int = REFILL_INTERVAL, token_cost: int = 1):
+async def consume_token(ip_address: str, bucket_size: int = BUCKET_SIZE, refill_rate: int = REFILL_AMOUNT, refill_interval: int = REFILL_INTERVAL, token_cost: int = 1):
     result = await run_consume_token_script(keys=[f"token_bucket:{ip_address}"], args=[bucket_size, refill_rate, refill_interval, token_cost])
 
     return bool(result)
