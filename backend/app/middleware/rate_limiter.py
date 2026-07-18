d8258bf53a274672eb5436cfba6cc1d00f8d1e58
Teddi <teddikao@gmail.com>
2026-07-18 14:29:55 +0800

    Add rate limit middleware

---
+++ backend/app/middleware/rate_limiter.py
@@ -0,0 +1,28 @@
+from fastapi import Request, JsonResponse
+
+from app.core.rate_limiter.rate_limiter import consume_token
+
+
+@app.middleware('http')
+async def rate_limit_middleware(request: Request, call_next):
+    client_ip = request.client.host
+
+    request_info = await consume_token(client_ip)
+    if not request_info["success"]:
+        return JsonResponse(
+            status_code=429,
+            content={"message": "Too many requests, please try again later."},
+            headers={
+                "Retry-After": str(request_info["retry_after"]),
+                "X-RateLimit-Limit": str(request_info["bucket_size"]),
+                "X-RateLimit-Remaining": str(request_info["remaining_tokens"]),
+                "X-RateLimit-Reset": str(request_info["reset_time"]),
+            }
+        )
+
+    response = await call_next(request)
+    response.headers["X-RateLimit-Limit"] = str(request_info["bucket_size"])
+    response.headers["X-RateLimit-Remaining"] = str(request_info["remaining_tokens"])
+    response.headers["X-RateLimit-Reset"] = str(request_info["reset_time"])
+
+    return response
