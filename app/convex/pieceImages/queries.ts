2708f42110994347660c7f043c5127e678bc4217
Teddi <teddikao@gmail.com>
2026-07-26 17:43:42 +0800

    Modify function to get URL instead of image ID

--- app/convex/pieceImages/queries.ts
+++ app/convex/pieceImages/queries.ts
@@ -1,13 +1,21 @@
 import { query } from "../_generated/server";
 import { v } from "convex/values";
 
-const getPieceImage = query({
+const getPieceImageUrl = query({
 	args: {
 		imageId: v.id("pieceImages"),
 	},
+
+	returns: v.union(v.string(), v.null()),
+
 	handler: async (ctx, args) => {
-		return await ctx.db.get("pieceImages", args.imageId);
+		const pieceImage = await ctx.db.get("pieceImages", args.imageId);
+		const storageId = pieceImage?.storageId;
+
+		if (!storageId) return null;
+
+		return await ctx.storage.getUrl(storageId);
 	},
 });
 
-export { getPieceImage };
+export { getPieceImageUrl };
