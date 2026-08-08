4187239c86ae12950c77077d8bf5e40ad4b477f7
Teddi <teddikao@gmail.com>
2026-08-08 13:57:13 +0800

    Comment out file to facilitate testing

--- backend/app/api/routers/json.py
+++ backend/app/api/routers/json.py
@@ -1,12 +1,12 @@
-from fastapi import APIRouter
-from app.schemas.normalise_json_request import NormaliseJSONRequest, NormaliseJSONResponse
-from app.engine.json_normaliser.json_normaliser import normalise_json as perform_json_normalisation
-
-router = APIRouter()
-
-@router.post("/normalise-json", response_model=NormaliseJSONResponse)
-async def normalise_json(request: NormaliseJSONRequest):
-	simple_json = request.simple_json
-	normalised_json = perform_json_normalisation(simple_json)
-
-	return NormaliseJSONResponse(normalised_json=normalised_json)
+# from fastapi import APIRouter
+# from app.schemas.normalise_json_request import NormaliseJSONRequest, NormaliseJSONResponse
+# from app.engine.json_normaliser.json_normaliser import normalise_json as perform_json_normalisation
+#
+# router = APIRouter()
+#
+# @router.post("/normalise-json", response_model=NormaliseJSONResponse)
+# async def normalise_json(request: NormaliseJSONRequest):
+# 	simple_json = request.simple_json
+# 	normalised_json = perform_json_normalisation(simple_json)
+#
+# 	return NormaliseJSONResponse(normalised_json=normalised_json)
